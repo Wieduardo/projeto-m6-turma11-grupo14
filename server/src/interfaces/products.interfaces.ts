26a915b0f8b01d783c7727b308e55e0c4db7b4cb
Willian Eduardo <wieduardo13@hotmail.com>
2022-12-19 12:41:37 -0300

    Criação da table e interface básica

---
+++ server/src/interfaces/products.interfaces.ts
@@ -0,0 +1,11 @@
+interface IProductRequest{
+    name: string,
+    description: string,
+    year: number,
+    kilometers: number,
+    ad_type: string,
+    price: string,
+    user: string,
+    vehicle_type: string,
+    images: string
+}
