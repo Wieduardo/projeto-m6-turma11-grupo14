74c0bbaa1a8dc952450cf8f2759bf8296348523b
Willian Eduardo <wieduardo13@hotmail.com>
2022-12-22 19:23:34 -0300

    Pequeno ajuste routes 2

--- server/src/routes/products.routes.ts
+++ server/src/routes/products.routes.ts
@@ -3,9 +3,9 @@ import { createProdController } from '../controllers/products.controllers'
 
 const productRoutes = Router()
 
+productRoutes.post('', createProdController)
 productRoutes.get('')
 productRoutes.get('/:prod_id')
-productRoutes.post('', createProdController)
 productRoutes.patch('/:prod_id')
 productRoutes.delete('/:prod_id')
 
