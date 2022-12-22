ff26f653a4754ac4560fa69a1b2108ac2fbd5ac2
Willian Eduardo <wieduardo13@hotmail.com>
2022-12-22 20:35:03 -0300

    Service, Controller e Route feitos

---
+++ server/src/services/products/listOneProd.service.ts
@@ -0,0 +1,19 @@
+import AppDataSource from '../../data-source'
+import { Product } from '../../entities/products.entities'
+import { AppError } from '../../errors/appError'
+import { IProductId } from '../../interfaces/products.interfaces'
+
+const listOneProdService = async ({prod_id}: IProductId) => {
+    
+    const prodRepository = AppDataSource.getRepository(Product)
+
+    const prod = await prodRepository.findOne({where: {id: prod_id}})
+
+    if(!prod){
+        throw new AppError(404,"Product does not exist")
+    }
+
+    return prod
+}
+
+export default listOneProdService
