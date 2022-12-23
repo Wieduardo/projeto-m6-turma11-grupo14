6be00eaf54db8033d2149aac484bbeddcd03c86b
Willian Eduardo <wieduardo13@hotmail.com>
2022-12-23 10:03:59 -0300

    Service, Controller e Route criados

---
+++ server/src/services/products/updateProd.service.ts
@@ -0,0 +1,36 @@
+import AppDataSource from '../../data-source'
+import { AppError } from '../../errors/appError'
+import { Product } from '../../entities/products.entities'
+import { IProductPatch } from '../../interfaces/products.interfaces'
+
+const patchProdService = async ({
+    prod_id, 
+    name, 
+    description,
+    year,
+    kilometers, 
+    ad_type,  
+    price, 
+    vehicle_type,
+    images 
+}: IProductPatch) =>{
+
+    const prodRepository = AppDataSource.getRepository(Product)
+
+    const { affected } = await prodRepository.update(prod_id, {
+        name, 
+        description,
+        year,
+        kilometers, 
+        ad_type,  
+        price, 
+        vehicle_type,
+        images})
+
+    if(affected === 0) throw new AppError(404, "Product not found");
+
+    return {message: "Product updated"};
+
+}
+
+export default patchProdService;
