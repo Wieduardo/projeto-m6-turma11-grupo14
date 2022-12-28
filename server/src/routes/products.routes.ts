c903796dbd40228eabee8cea35812588bef5245a
Bruno <brunopassos_bp@hotmail.com>
2022-12-28 13:40:47 -0300

    feat: implemented list user products only

--- server/src/routes/products.routes.ts
+++ server/src/routes/products.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
-import { createProdController, deleteProdController, listProdController, patchProdController, retrieveUserController } from '../controllers/products.controllers'
+import { createProdController, deleteProdController, listProdController, listUserProductsController, patchProdController, retrieveUserController } from '../controllers/products.controllers'
 
 const productRoutes = Router()
 
 productRoutes.post('', createProdController)
 productRoutes.get('', listProdController)
 productRoutes.get('/:prod_id', retrieveUserController)
+productRoutes.get("/user/:user_id", listUserProductsController)
 productRoutes.patch('/:prod_id', patchProdController)
 productRoutes.delete('/:prod_id', deleteProdController)
 
