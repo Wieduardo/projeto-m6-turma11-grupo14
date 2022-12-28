36119077c6474b099143d0385c30665d9e1be3d9
Willian Eduardo <wieduardo13@hotmail.com>
2022-12-28 17:07:13 -0300

    Configurado cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata"
 import "express-async-errors"
+import cors from 'cors'
 import express from 'express'
 import productRoutes from "./routes/products.routes"
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/products', productRoutes)
