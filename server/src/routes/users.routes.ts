2e7d4a46d11d93814f24f86a97e5e68c97834682
Willian Eduardo <wieduardo13@hotmail.com>
2023-01-02 18:27:59 -0300

    Gerada rota de users

---
+++ server/src/routes/users.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+
+const userRoutes = Router()
+
+userRoutes.post('')
+userRoutes.get('')
+userRoutes.patch('')
+userRoutes.delete('')
+userRoutes.post('/login')
+userRoutes.get('/user')
+
+export default userRoutes
