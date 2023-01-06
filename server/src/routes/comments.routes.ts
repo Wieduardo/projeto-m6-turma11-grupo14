b1fd053262165bbb0d41b6a6f378369500a4d93c
arthur-r-goncalves <arthur.r.goncalves@gmail.com>
2023-01-06 16:29:49 -0300

    fix: correção rota

--- server/src/routes/comments.routes.ts
+++ server/src/routes/comments.routes.ts
@@ -4,8 +4,8 @@ import { createCommentController } from '../controllers/comment.controllers'
 const commentRoutes = Router()
 
 commentRoutes.post('', createCommentController)
-commentRoutes.get('/comment_id')
-commentRoutes.patch('/comment_id')
-commentRoutes.delete('/comment_id')
+commentRoutes.get('/:comment_id')
+commentRoutes.patch('/:comment_id')
+commentRoutes.delete('/:comment_id')
 
 export default commentRoutes
